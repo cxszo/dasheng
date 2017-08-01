d27b8f0bfec000a022913379e8df7fca4bcf86ed
Lzs <liuzhishan@9188.com>
2017-08-01 09:38:22 +0800

    首页大致结构，还需要很多完善

---
+++ js/index.js
@@ -0,0 +1,18 @@
+/*** Created by Lzs on 2017/7/14.*/
+var invite = (function () {
+    var g ={
+       
+    }
+    var o = {
+        render:function(){
+        },
+        bind:function(){
+       
+        },
+        init:function(){
+            
+         }
+    }
+    return {o:o,g:g}
+})()  
+invite.o.init();
